0a51b14e3644d000c790feba6b20e0771309a68e
Suprita-2003 <sanadanisuprita@gmail.com>
2025-05-05 23:16:07 +0530

    Second Commit

--- Demo-git/src/Demo/GitDemo.java
+++ Demo-git/src/Demo/GitDemo.java
@@ -2,6 +2,7 @@ package Demo;
 
 public class GitDemo {
        public static void main(String[] args) {
-System.out.println("Git Demo");    	   
+System.out.println("Git Demo"); 
+System.out.println("Hello");
        }
 }
